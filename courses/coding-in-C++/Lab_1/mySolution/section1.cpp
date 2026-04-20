9c05d69ef30e8d006025929712ee0e9985f85bfa
alexander-trippel <trippelalexander1@gmail.com>
2026-04-20 09:31:48 +0000

    section1

---
+++ courses/coding-in-C++/Lab_1/mySolution/section1.cpp
@@ -0,0 +1,13 @@
+#include "header.hpp"
+
+void printFromHeader()
+{
+    std::cout << "Hello from .cpp" << std::endl;
+}
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    printFromHeader();
+}
